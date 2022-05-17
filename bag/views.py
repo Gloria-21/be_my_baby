0024ae3fd75bc4c30cd732d565d07f11b99e5b8d
Gloria-21 <gloriahn_82@hotmail.com>
2022-05-17 21:03:58 +0000

    Added toast, views, notification css, shppong bag preview

--- bag/views.py
+++ bag/views.py
@@ -14,14 +14,17 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified producto to the shopping bag"""
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Update {product.name} quantity to {bag.item_id}')
     else:
         bag[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your bag')
         
     request.session['bag'] = bag
     return redirect(redirect_url)
@@ -51,6 +54,7 @@ def remove_from_bag(request, item_id):
         product = get_object_or_404(Product, pk=item_id)
         bag = request.session.get('bag', {})
         bag.pop(item_id)
+        messages.success(request, f'Remove {product.name} from your bag')
             
         request.session['bag'] = bag
         return HttpResponse(status=200)
