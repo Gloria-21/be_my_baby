2d586901eeb6b2c40df028998a84ce69ebdd41db
Gloria-21 <gloriahn_82@hotmail.com>
2022-05-18 21:22:31 +0000

    Added checkout success logic

--- checkout/urls.py
+++ checkout/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     # we take the order number as an argument and change view and name to checkout_success
+    path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),
 ]
