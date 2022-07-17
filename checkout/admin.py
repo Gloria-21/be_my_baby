c0a78a29b418de44e9df00498ba28ad371be6e92
Gloria-21 <gloriahn_82@hotmail.com>
2022-07-17 22:14:24 +0000

    added order history and updated checkout views

--- checkout/admin.py
+++ checkout/admin.py
@@ -20,7 +20,7 @@ class OrderAdmin(admin.ModelAdmin):
                        'stripe_pid')
 
     """Allow us to specify the order for the fields in the admin interface"""
-    fields = ('order_number', 'date', 'full_name',
+    fields = ('order_number', 'user_profile', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
