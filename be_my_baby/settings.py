aeae29de9f30b4d3c8ac1d0715fd9cfd4cd65253
Gloria-21 <gloriahn_82@hotmail.com>
2022-07-27 14:57:20 +0000

    Creating blog section

--- be_my_baby/settings.py
+++ be_my_baby/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     'profiles',
+    'blog',
 
     # Others
     'crispy_forms',
---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, redirect, reverse
+
+from .models import Post
+
+
+def blog(request):
+    posts = Post.objects.all()
+    template = 'blog/blog.html'
+    context = {'posts': posts}
+
+    return render(request, template, context)
+
